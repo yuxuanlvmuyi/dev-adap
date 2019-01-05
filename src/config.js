4dc1b1d19a3333deb34bc619ee2cc89e8805ba81
liu <liu@example.com>
2019-01-05 08:01:51 +0800

    change for bug

--- src/config.js
+++ src/config.js
@@ -49,8 +49,8 @@ export let smartContract = {
   // assistantBase58Address : 'TE21miaHFLdgXYaNF4dinLxKJFumQn2yvC',
 
   developerAddress : 'TRXz1kqRyZuADGY3DwYdfwj2qC7v9RWtjX', // 开发者账户地址
-  developerPrivateKey : '2162858F1651E51713D4CEBF5D148AC06C3516C35EAA4800C038B67BBEF3B399', // 开发者私钥
-  assistantPrivateKey : '2162858F1651E51713D4CEBF5D148AC06C3516C35EAA4800C038B67BBEF3B399', // assistant私钥
+  developerPrivateKey : '8EBE01BA3E1E74ED6A0BCD4C3320D0B9D3EB20B173DB190563EB90AAD0405523', // 开发者私钥
+  assistantPrivateKey : '8EBE01BA3E1E74ED6A0BCD4C3320D0B9D3EB20B173DB190563EB90AAD0405523', // assistant私钥
   fullNodeUrl : 'https://api.trongrid.io',
   solidityNodeUrl : 'https://api.trongrid.io',
   eventServerUrl : 'https://api.trongrid.io',
